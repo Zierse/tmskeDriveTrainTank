fdbde447d4a85b0bd4879432cce71faf8de88d49
zierse <thiagozierse@gmail.com>
2025-03-11 09:41:29 -0400

    Atualização usando pigeon 2.0

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.core.CorePigeon2;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -11,8 +13,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -27,21 +31,40 @@ public class Robot extends TimedRobot {
   SparkMax motorInferiorEsquerdoSeg;
   Joystick logitech;
 
+  private static final double AJUSTE_ANALOG = 0.5;
+  private static final int CURRENT_LIMIT = 30;
+
+  private static final int JOYSTICK_PORT = 0;
+
+  private static final int MOTOR_SUPERIOR_ESQUERDO_LIDER = 2;
+  private static final int MOTOR_INFERIOR_ESQUERDO_SEG = 3;
+  private static final int MOTOR_SUPERIOR_DIREITO_LIDER = 4;
+  private static final int MOTOR_INFERIOR_DIREITO_SEG = 5;
+
+  // Definindo constantes para os índices dos eixos
+  private static final int AXIS_GATILHO_ESQUERDO = 2;
+  private static final int AXIS_GATILHO_DIREITO = 3;
+  private static final int AXIS_ANALOGICO_ESQUERDO = 0;
+
+  private CorePigeon2 gyro;
+
   public Robot() {
-    
-    motorSuperiorEsquerdoLider = new SparkMax(2, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
-    motorInferiorEsquerdoSeg = new SparkMax(3, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
-    motorSuperiorDireitoLider = new SparkMax(4, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
-    motorInferiorDireitoSeg = new SparkMax(5, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
+
+    gyro = new CorePigeon2(21); //Definindo o Gyro como um CorePigeon2 para um Pigeon 2.0
+
+    motorSuperiorEsquerdoLider = new SparkMax(MOTOR_SUPERIOR_ESQUERDO_LIDER, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
+    motorInferiorEsquerdoSeg = new SparkMax(MOTOR_INFERIOR_ESQUERDO_SEG, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
+    motorSuperiorDireitoLider = new SparkMax(MOTOR_SUPERIOR_DIREITO_LIDER, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
+    motorInferiorDireitoSeg = new SparkMax(MOTOR_INFERIOR_DIREITO_SEG, MotorType.kBrushless); //Estanciando variaveis com os parametros do motores.
 
     SparkMaxConfig configGlobal = new SparkMaxConfig(); //Estaciando as configurações para motores
     SparkMaxConfig configMotorDireitoLider = new SparkMaxConfig();
-    SparkMaxConfig configMotorEsquerdoSeg = new SparkMaxConfig();
     SparkMaxConfig configMotorDireitoSeg = new SparkMaxConfig();
+    SparkMaxConfig configMotorEsquerdoSeg = new SparkMaxConfig();
 
     //Definindo Configurações
     configGlobal
-        .smartCurrentLimit(30)
+        .smartCurrentLimit(CURRENT_LIMIT)
         .idleMode(IdleMode.kBrake);
 
     configMotorDireitoLider
@@ -57,13 +80,13 @@ public class Robot extends TimedRobot {
         .apply(configGlobal)
         .follow(motorSuperiorDireitoLider);
 
-    //Parametrizzando as configurações
+    //Parametrizando as configurações
         motorSuperiorEsquerdoLider.configure(configGlobal, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         motorInferiorEsquerdoSeg.configure(configMotorEsquerdoSeg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         motorSuperiorDireitoLider.configure(configMotorDireitoLider, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         motorInferiorDireitoSeg.configure(configMotorDireitoSeg, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-        logitech = new Joystick(0); //Estaciando o Joystick
+        logitech = new Joystick(JOYSTICK_PORT); //Estaciando o Joystick
   }
 
   @Override
@@ -76,24 +99,34 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
   public void teleopPeriodic() {
-    
-    double gatEsquerdo = -logitech.getRawAxis(2);
-    double gatDireito = -logitech.getRawAxis(3);
 
-    double analogEsquerdo = logitech.getRawAxis(0);
+    StatusSignal<Angle> yaw = gyro.getYaw(); //Angulo de rotação do eixo Z
+    StatusSignal<Angle> pitch = gyro.getPitch(); //Angulo de rotação do eixo Y
+    StatusSignal<Angle> roll = gyro.getRoll(); //Angulo de rotacao do eixo X
+    
+    double gatEsquerdo = -logitech.getRawAxis(AXIS_GATILHO_ESQUERDO);
+    double gatDireito = -logitech.getRawAxis(AXIS_GATILHO_DIREITO);
+
+    double analogEsquerdo = logitech.getRawAxis(AXIS_ANALOGICO_ESQUERDO);
 
     double valorGat = gatDireito - gatEsquerdo;
-    double ajusteAnalogEsquerdo = analogEsquerdo * 0.5;
+    double ajusteAnalogEsquerdo = analogEsquerdo * AJUSTE_ANALOG;
 
     motorSuperiorEsquerdoLider.set(valorGat + ajusteAnalogEsquerdo);
-    motorInferiorEsquerdoSeg.set(valorGat + ajusteAnalogEsquerdo);
-
     motorSuperiorDireitoLider.set(valorGat - ajusteAnalogEsquerdo);
-    motorInferiorDireitoSeg.set(valorGat - ajusteAnalogEsquerdo);
+
+
+    //SmartDashboard.putNumber("Rotacao do Eixo Z Yaw", yaw.getValueAsDouble());
+    //SmartDashboard.putNumber("Rotacao do Eixo Y Pitch", pitch.getValueAsDouble());
+    //SmartDashboard.putNumber("Rotacao do Eixo X Roll", roll.getValueAsDouble());
+
+    
+
   }
 
   @Override
@@ -101,7 +134,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {}
-
+ 
   @Override
   public void testInit() {}
 
